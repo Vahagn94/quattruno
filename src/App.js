0eaaf19d7765a3c915b064b54c15cfa230ac6508
Surinder Singh <surindersingh9495@gmail.com>
2018-10-22 13:40:32 -0700

    Navbar & Card added
    Did not use react to make components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button bsStyle='danger'>Hello</Button>
-        </header>
-      </div>
-    );
+  return (
+  <div className="App">
+     <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+           <div className="container-fluid">
+              <div className="navbar-header">
+                 <h1 style={{color:"#FFFFFF"}}>CSUNHub</h1>
+              </div>
+
+           </div>
+        </nav>
+     </div>
+
+
+
+     <div className="container">
+        <div className="row">
+           <div className="col-sm-6">
+              <div className="card">
+                 <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                 </div>
+              </div>
+           </div>
+           <div className="col-sm-6">
+              <div className="card">
+                 <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </div>
+  </div>
+  );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+//import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
