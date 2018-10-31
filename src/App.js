8c98dc04b11ecb511cfe44f30f5538ddfe9af94e
Surinder Singh <surindersingh9495@gmail.com>
2018-10-31 00:17:10 -0700

    Column Layout with Panels
    Whole panel is clickable
    Added scrolling for tighter windows
    Rounded buttons
    un-underline text in panel (anchor links)

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary Shadow">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary Shadow" >
           <div className="container-fluid">
             <div className="navbar-header">
               <h1 style={{ color: "#FFFFFF", fontSize: 30 }}>CSUN Hub</h1>
             </div>
           </div>
         </nav>
-        <div className="container">
+        <div className="container-fluid" id="container-scroll">
           <div className="row">
-            <div className="col-sm-6">
-              <Card title="Books" message="Buy & Sell Books" />
+            <div className="col-sm-6 col-lg-3">
+              { /* insert navigation in this column */ }
             </div>
-            <div className="col-sm-6">
-              <Card title="Notes" message="Upload & View Notes" />
+            <div className="col-sm-6 col-lg-3">
+              <a href="/" className="pagerLink">
+                <Card title="Books" message="Buy and Sell Books" />
+              </a>
+            </div>
+            <div className="col-sm-12 col-lg-3">
+              <a href="/" className="pagerLink">
+                <Card title="Notes" message="Upload & Download Class Notes" />
+              </a>
+            </div>
+            <div className="col-sm-12 col-lg-3">
+              <a href="/" className="pagerLink">
+                <Card title="Ridesharing" message="Carpool with Classmates" />
+              </a>
             </div>
           </div>
         </div>
