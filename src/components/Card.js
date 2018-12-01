40fc7b43629edb04c56964bc5818ac109bcf36cf
Surinder Singh <surindersingh9495@gmail.com>
2018-12-01 15:51:32 -0800

    Nested Listings into Book Card
    card-deck class is miniziming the cards as you add each additional one which is not what we want

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,18 @@ import "../App.css";
 class Card extends Component {
   state = {
     title: this.props.title,
-    message: this.props.message
+    message: this.props.message,
+    body: this.props.body,
+    height: this.props.height
   };
 
   render() {
     return (
-      <div className="card" style={{height: "48rem", borderRadius: "5px"}}>
+      <div className="card" style={{height: this.props.height, borderRadius: "5px"}}>
         <div className="card-body" style={{paddingTop: "3rem"}}>
           <h5 className="card-title" style={{fontSize: "2rem"}}>{this.props.title}</h5>
           <p className="card-text" style={{paddingTop: "1rem"}}>{this.props.message}</p>
+          <div> {this.props.body} </div>
         </div>
       </div>
     );
