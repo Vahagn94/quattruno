9b081e61517a6e7b9af720cdd4d6d2f1c2afedaa
Abdulla <31869773+Abdulla155@users.noreply.github.com>
2018-12-01 20:25:34 -0800

    Scrolling bar attempt

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -7,19 +7,25 @@ class Books extends Component {
   render() {
     return (
       <div className="container-fluid" id="container-scroll">
-          <div className="col-lg-12">
-            <Card title="Books" height="55rem"
-              body =
-              <div class="scrolling-wrapper">
-                <div className="card-deck" style={{paddingTop: "5rem"}}>
-                  <Card title="Intro to Operating Systems" message="322" height="30rem"/>
-                  <Card title="Data Mining" message="484"/>
-                  <Card title="Combinatorial Algorithms" message="482"/>
-                  <Card title="Discrete Structures" message="252"/>
+        <div className="col-lg-12">
+          <Card
+            title="Books"
+            height="55rem"
+            body={
+              <div>
+                <div class="scrolling-wrapper cards">
+                  <Card title="1 Intro to Operating Systems" message="322" />
+                  <Card title="2 Data Mining" message="484" />
+                  <Card title="3 Combinatorial Algorithms" message="482" />
+                  <Card title="4 Discrete Structures" message="252" />
+                  <Card title="5 Data Mining" message="484" />
+                  <Card title="6 Combinatorial Algorithms" message="482" />
+                  <Card title="7 Discrete Structures" message="252" />
                 </div>
               </div>
-            />
-          </div>
+            }
+          />
+        </div>
       </div>
     );
   }
