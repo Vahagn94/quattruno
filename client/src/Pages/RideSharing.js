592a32deab912c56655dff07b8c9032481b94636
Surinder Singh <surindersingh9495@gmail.com>
2019-02-27 15:37:08 -0800

    Added ridesharing tab

---
+++ client/src/Pages/RideSharing.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../App.css";
+import Card from "../Components/Card.js";
+
+class RideSharing extends Component {
+  render() {
+    return (
+      <div className="container-fluid" id="container-scroll">
+          <div className="col-lg-12">
+            <Card title="Ridesharing" height="55rem"/>
+          </div>
+      </div>
+    );
+  }
+}
